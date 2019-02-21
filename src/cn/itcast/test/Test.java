da412fd323f891434768ae693c57fade037ad542
zhixian <zhixian@163.com>
2019-02-21 21:24:30 +0800

    添加test工程至git

---
+++ src/cn/itcast/test/Test.java
@@ -0,0 +1,10 @@
+package cn.itcast.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello zhixian...");
+        System.out.println("hello zhixian...");
+        System.out.println("hello zhixian...");
+        System.out.println("hello zhixian...");
+    }
+}
